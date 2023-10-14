efd0169dc66255022c7c3ca3c87b8862b59539bc
Venus Amor Sablay <venusamorsablay@gmail.com>
2023-10-14 17:19:06 +0800

    Add untracked file

---
+++ database/migrations/2023_10_12_095007_update_intern_job_prefences_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('intern_job_preferences',function (Blueprint $table){
+            $table->dropColumn("job_preference");
+            $table->longText('evaluation');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
