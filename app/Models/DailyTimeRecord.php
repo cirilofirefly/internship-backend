58bdcc32d9a8de38d555d808786141d043009dff
Cirilo Bucatcat <bucatcat1999@gmail.com>
2024-04-15 10:20:36 +0800

    evaluation test

--- app/Models/DailyTimeRecord.php
+++ app/Models/DailyTimeRecord.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class DailyTimeRecord extends Model
 {
     const VALIDATED = 'validated';
-    const TOTAL_HOURS = 486;
+    const TOTAL_HOURS = 486; //Change this to 0 for demo
 
     protected $table = 'daily_time_records';
     protected $fillable = [
